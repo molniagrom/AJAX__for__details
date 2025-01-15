c5a527f74ef104058a7b5f4f23e2446162e2c195
molnia_grom <mgrom250@gmail.com>
2025-01-15 19:08:57 +0200

    I watched Lesson 2 from the notes.

--- 02/script.js
+++ 02/script.js
@@ -1,16 +1,18 @@
-let n = 8
+console.log(0)
 
 $.ajax({
-    url: 'https://jsonplaceholder.typicode.com/photos',
+    url: 'https://picsum.photos/v2/list',
     method: 'GET',
-    success: function (data) {
-        // Выводим первые 10 результатов
-        console.log(data.slice(0, 10));
+    success: function(data){
+        data.slice(0, 5).forEach(image => {
+            $("body").append(`<img src="${image.download_url}" alt="${image.author}" style="width:200px; margin:10px;">`);
+        });
     },
-    error: function (error) {
-        console.error('Ошибка:', error);
+    error: function(data){
+        console.error("Error:",data);
     }
 });
+
 
 n = 5
 console.log(n)
